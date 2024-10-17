66d535e4dcdda62f2d955637350143423785eef3
Matthew Dixon <m.dixon5@outlook.com>
2024-10-17 16:17:38 +0000

    Add calculate correct answer function for addition game

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,8 +42,23 @@ function checkAnswer(){
 
 }
 
-function calculateCoreectAnswer(){
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
+function calculateCorrectAnswer(){
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator");
 
+    if (operator === '+'){
+        return [operand1 + operand2, "addition"];
+    }
+    else {
+        alert(`unimplemented operator ${operator}`);
+        throw `unimplemented operator ${operator}. aborting`;
+    }
+    
 }
 
 function incrementScore(){
